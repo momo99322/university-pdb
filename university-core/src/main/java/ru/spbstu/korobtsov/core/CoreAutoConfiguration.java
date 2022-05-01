284429c755344493cad7ddbcdf96ac673d5f419e
Daniil Korobtsov <dankorobzov@yandex.ru>
2022-05-01 10:44:21 +0300

    separate external and internal configurations + rename html folder html->templates

--- university-core/src/main/java/ru/spbstu/korobtsov/core/CoreAutoConfiguration.java
+++ university-core/src/main/java/ru/spbstu/korobtsov/core/CoreAutoConfiguration.java
@@ -1,14 +1,28 @@
 package ru.spbstu.korobtsov.core;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories
+@EnableConfigurationProperties
 @EntityScan(basePackages = "ru.spbstu.korobtsov.api.domain")
 @ComponentScan(basePackageClasses = CoreAutoConfiguration.class)
 public class CoreAutoConfiguration {
+
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "university.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
 }
 
